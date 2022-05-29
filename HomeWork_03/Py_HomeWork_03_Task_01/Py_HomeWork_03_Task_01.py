4c9de414b9e49a421d9d19400e879eab04d55c91
FominaCathy <99419802+FominaCathy@users.noreply.github.com>
2022-05-29 10:08:10 +0400

    добавли решение задачи 2 с помощью радя Нилоканта

---
+++ HomeWork_03/Py_HomeWork_03_Task_01/Py_HomeWork_03_Task_01.py
@@ -0,0 +1,43 @@
+﻿# Найти НОК двух чисел
+
+
+def simple_divisors(number): # функция разложения на простые множители
+    simple_div = {}
+
+    i= 2
+    while number != 1:
+        if number % i == 0:
+            if i in simple_div:
+                simple_div[i] = simple_div.get(i) + 1
+            else:
+                simple_div[i] =  1
+            number /= i
+        else:
+            i += 1
+
+    return simple_div
+
+
+
+def search_nok(number_first, number_second):
+    nok = 1
+    # разложим на простые множители оба числа
+
+    simple_div_first = simple_divisors(number_first)
+    simple_div_second = simple_divisors(number_second)
+
+    for i in simple_div_first:
+        if i in simple_div_second:
+            nok *= i ** max(simple_div_first.get(i), simple_div_second.pop(i))
+        else:
+            nok *= i ** simple_div_first.get(i)
+    
+    for i in simple_div_second:
+        nok *= i ** simple_div_second.get(i)
+
+    return nok
+
+
+print(f"НОК чисел 12 и 48 = {search_nok (12, 48)}")
+
+print(f"НОК чисел 130 и 15 = {search_nok(130, 15)}")
---
+++ HomeWork_03/Py_HomeWork_03_Task_02/Py_HomeWork_03_Task_02.py
@@ -0,0 +1,24 @@
+﻿# Вычислить число Пи c заданной точностью d
+# Пример: при d = 0.001,  c= 3.141.
+
+# Ряд Нилаканта: π = 3 + 4/(2*3*4) — 4/(4*5*6) + 4/(6*7*8) — 4/(8*9*10) + 4/(10*11*12) — 4/(12*13*14) …
+
+
+def calc_pi (difference : float): 
+    number_pi = 3
+    
+    i = 2
+    j = 0
+
+    while (4/(i*(i+1)*(i+2))) > difference:
+        number_pi += ((-1) ** j) * (4 /(i*(i+1)*(i+2)))
+        i += 2
+        j += 1
+
+    number_pi += ((-1) ** j) * (4 /(i*(i+1)*(i+2)))
+
+    return round(number_pi, (len(str(difference)))-2)
+
+diff = 0.0001
+
+print(f"число Пи с точностью {diff} = {calc_pi(diff)}")
